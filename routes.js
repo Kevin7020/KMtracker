f1f337fcdb4be8e79bd776fc3dfd0cef23791c3f
Kevin Porras R <kevin702010@hotmail.com>
2018-04-12 07:50:24 -0600

    Signup disabled for testing on public

--- routes.js
+++ routes.js
@@ -35,11 +35,14 @@ module.exports = function(app, passport) {
     // SIGNUP ==============================
     // =====================================
     // show the signup form
+
+    /* SIGNUP PAGE DISABLED !
     app.get('/signup', function(req, res) {
 
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
+    */
 
     // process the signup form
     // app.post('/signup', do all our passport stuff here);
