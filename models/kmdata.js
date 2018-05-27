c6a6b7b52e1a51b7c933b912c5e218f5448ab594
Kevin Porras R <kevin702010@hotmail.com>
2018-05-27 08:19:26 -0600

    Added KMlogs collection name to the kmdata schema

--- models/kmdata.js
+++ models/kmdata.js
@@ -2,16 +2,22 @@
 // load the things we need
 var mongoose = require('mongoose');
 
-// define the schema for our km model
-var kmSchema = mongoose.Schema({
-
+/*
+*Define the schema for our km model
+*and add the name of the mongodb collection.
+*/
+var kmSchema = mongoose.Schema(
+  {
     Kmetros: String,
     litros: String,
     precioT: String,
     precioL: String,
     fecha: String,
-
-});
+  },
+  {
+    collection: 'KMlogs'
+  }
+);
 
 // methods ======================
 //None by now
